fc36df7609fcad0621378be22a6274bddffed9c9
TheAesirDev <boludedaniel@gmail.com>
2023-05-04 03:00:18 +0100

    changed the font to gilroy regular

--- src/Reusables/constants.js
+++ src/Reusables/constants.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const headerFont = 'Clash Display Semibold,sans-serif'
-export const bodyFont = 'Gilroy-Medium,sans-serif'
+export const bodyFont = 'Gilroy-Regular,sans-serif'
 export const subHeadingFontSize = 'clamp(1.8rem,5vw,2.3rem)'
 export const heroFontSize = 'clamp(3rem,5vw,4rem)'
 export const baseUrl =  import.meta.env.VITE_BASE_URL
